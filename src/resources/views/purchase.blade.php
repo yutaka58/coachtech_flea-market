baf7a9c39dad7f83af398bea2ca1b57e77a6fe55
yutaka <yu.tama.5@icloud.com>
2026-01-03 00:38:22 +0900

    商品購入画面作成中

--- src/resources/views/purchase.blade.php
+++ src/resources/views/purchase.blade.php
@@ -1,19 +1,40 @@
 @extends('layouts/app')
 
 @section('css')
-<link rel="stylesheet" href="{{ asset('css/auth/profile.css') }}">
+<link rel="stylesheet" href="{{ asset('css/purchase.css') }}">
 @endsection
 
 
 @section('content')
-<div class="container">
-    <h1>購入画面</h1>
-    <div class="product-info">
-        <img src="{{ asset($product->img_url) }}" width="200">
-        <h2>{{ $product->name }}</h2>
-        <p>価格：￥{{ number_format($product->price) }}</p>
+
+<div class="product-container">
+    <div class="product-visual">
+        <div class="product-image-wrapper">
+            <img src="{{ asset($product->img_url) }}" class="product-image">
+        </div>
+        <div class="product-details">
+            <h1 class="product-name"> {{ $product->name }}</h1>
+            <p class="product-price">￥{{ number_format($product->price) }}<span>(税込)</span></p>
+        </div>
     </div>
-    
+    <div class="product-visual">
+        <div class="product-details">
+            <p class="product-header">支払方法</p>
+            <select class="payment-method" name="" id="" >選択してください</select>
+        </div>
+    </div>
+    <div class="product-visual">
+        <div class="product-details">
+            <p class="product-header">配送先
+            <a class="address-change" href="/purchase/address/{item_id}" >変更する</a>
+        </div>
+        <div class="">
+
+        </div>
+    </div>
+
+
+
     {{-- ここに支払い方法の選択や住所確認などのフォームを実装していきます --}}
     <button class="btn-primary">購入する</button>
 </div>
